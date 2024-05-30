773ee11c953e301fc70be70f34f789d370bf3ee7
Sushant Shinde <sushant.165.shinde@gmail.com>
2024-05-30 23:22:03 +0530

    Fix attendance response issue

---
+++ src/main/java/com/project/attendance/Payload/Response/AttendanceResponse.java
@@ -0,0 +1,19 @@
+package com.project.attendance.Payload.Response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AttendanceResponse {
+    List<LocalDate> presentDays ;
+    int maxStreak ;
+}
